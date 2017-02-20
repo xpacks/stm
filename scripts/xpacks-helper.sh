c7fe44837b271996e1f21df8495fde4909eae363
Liviu Ionescu <ilg@livius.net>
2017-02-20 23:20:39 +0200

    add do_add_stm32_xpack()
    - remove "f3" case from cmsis

--- scripts/xpacks-helper.sh
+++ scripts/xpacks-helper.sh
@@ -25,12 +25,6 @@ do_add_stm32_cmsis_xpack() {
 
   local include_path="${pack_folder}/Drivers/CMSIS/Device/ST/STM32${family_uc}xx/Include"
   local src_path="${pack_folder}/Drivers/CMSIS/Device/ST/STM32${family_uc}xx/Source/Templates"
-  if [ "${family}" == "f3" ]
-  then
-    # Some Keil packages have different paths.
-    include_path="${pack_folder}/Device/Include"
-    src_path="${pack_folder}/Device/Source"
-  fi
 
   do_prepare_dest "${pack_name}/${device}/include"
   do_add_content "${include_path}/cmsis_device.h" 
@@ -92,6 +86,52 @@ do_add_stm32_hal_xpack() {
   do_prepare_dest "${pack_name}/src"
   do_add_content "${pack_folder}/Drivers/STM32${family_uc}xx_HAL_Driver/Src"/* 
 
+  echo "Removing '${dest_folder}/*_template.c'..."
+  rm "${dest_folder}/"*_template.c
+}
+
+# -----------------------------------------------------------------------------
+
+# $1 = device name suffix (like "stm32f407xx")
+# $2 (optional) = scope (like ilg, lix, ...)
+do_add_stm32_xpack() {
+  local device=$(echo $1 | tr '[:upper:]' '[:lower:]')
+  local family=${device:5:2}
+  local family_uc=$(echo ${family} | tr '[:lower:]' '[:upper:]')
+  local scope="ilg"
+  if [ $# -ge 2 ]
+  then
+    scope="$2"
+  fi
+
+  local pack_name="stm32${family}"
+
+  do_tell_xpack "${pack_name}-xpack"
+
+  do_select_pack_folder "${scope}/${pack_name}.git"
+
+  local include_path="${pack_folder}/Drivers/CMSIS/Device/ST/STM32${family_uc}xx/Include"
+  local src_path="${pack_folder}/Drivers/CMSIS/Device/ST/STM32${family_uc}xx/Source/Templates"
+
+  # Copy CMSIS support.
+  do_prepare_dest "${pack_name}/${device}/include"
+  do_add_content "${include_path}/cmsis_device.h" 
+  do_add_content "${include_path}/stm32${family}xx.h" 
+  do_add_content "${include_path}/${device}.h" 
+  do_add_content "${include_path}/system_stm32${family}xx.h" 
+
+  do_prepare_dest "${pack_name}/${device}/src"
+  do_add_content "${src_path}/system_stm32${family}xx.c" 
+  do_add_content "${src_path}/gcc/vectors_${device}.c" 
+
+  # Copy HAL support.
+  do_prepare_dest "${pack_name}/hal/include"
+  do_add_content "${pack_folder}/Drivers/STM32${family_uc}xx_HAL_Driver/Inc"/* 
+
+  do_prepare_dest "${pack_name}/hal/src"
+  do_add_content "${pack_folder}/Drivers/STM32${family_uc}xx_HAL_Driver/Src"/* 
+
+  # Clean unwanted files.
   echo "Removing '${dest_folder}/*_template.c'..."
   rm "${dest_folder}/"*_template.c
 }
